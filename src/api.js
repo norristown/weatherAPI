0d8acb3042fc99b95766a9611bcfd44af7581d19
norristown <jarvasity@gmail.com>
2023-08-26 19:11:38 -0400

    added zipcode functionality

--- src/api.js
+++ src/api.js
@@ -1,15 +1,19 @@
 const getWeather = require("./getWeather");
 export async function checkWeather(city) {
   const apiKey = "1a958c20fc9465e8ef2c3dcbf82e0bcd";
-  const apiUrl =
+  const apiUrlCity =
     "https://api.openweathermap.org/data/2.5/weather?units=imperial&q=";
-  const responseCity = await fetch(apiUrl + city + `&appid=${apiKey}`);
+  const responseCity = await fetch(apiUrlCity + city + `&appid=${apiKey}`);
   var dataCity = await responseCity.json();
 
+  const apiUrlZip =
+    "https://api.openweathermap.org/data/2.5/weather?units=imperial&zip=";
+  const responseZip = await fetch(apiUrlZip + city + `&appid=${apiKey}`);
+  var dataZip = await responseZip.json();
+
   if (/^\d+$/.test(city)) {
+    getWeather.getWeather(responseZip, dataZip);
   } else {
     getWeather.getWeather(responseCity, dataCity);
-
-    console.log(dataCity);
   }
 }
