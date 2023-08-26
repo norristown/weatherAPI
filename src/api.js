61f911860755d33d46900a9bb73885d20dd7b6c7
norristown <jarvasity@gmail.com>
2023-08-26 19:21:28 -0400

    would add weekly forecast or something similar but api I used doesn't allow it so I think I'm done

--- src/api.js
+++ src/api.js
@@ -1,19 +1,18 @@
 const getWeather = require("./getWeather");
 export async function checkWeather(city) {
   const apiKey = "1a958c20fc9465e8ef2c3dcbf82e0bcd";
-  const apiUrlCity =
-    "https://api.openweathermap.org/data/2.5/weather?units=imperial&q=";
-  const responseCity = await fetch(apiUrlCity + city + `&appid=${apiKey}`);
-  var dataCity = await responseCity.json();
-
-  const apiUrlZip =
-    "https://api.openweathermap.org/data/2.5/weather?units=imperial&zip=";
-  const responseZip = await fetch(apiUrlZip + city + `&appid=${apiKey}`);
-  var dataZip = await responseZip.json();
 
   if (/^\d+$/.test(city)) {
+    const apiUrlZip =
+      "https://api.openweathermap.org/data/2.5/weather?units=imperial&zip=";
+    const responseZip = await fetch(apiUrlZip + city + `&appid=${apiKey}`);
+    var dataZip = await responseZip.json();
     getWeather.getWeather(responseZip, dataZip);
   } else {
+    const apiUrlCity =
+      "https://api.openweathermap.org/data/2.5/weather?units=imperial&q=";
+    const responseCity = await fetch(apiUrlCity + city + `&appid=${apiKey}`);
+    var dataCity = await responseCity.json();
     getWeather.getWeather(responseCity, dataCity);
   }
 }
